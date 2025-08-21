68cd9cbe81a6087b8af7c94ed38640a952af2197
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2025-08-21 10:27:55 +0200

    Fix: localhost address must be 0.0.0.0

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import express from "express"
 import { env } from "node:process"
 
 const app = express()
-const port = env.PORT || 3000
+const port: number = Number(env.PORT) || 3000 // Could crash
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello world" })
 })
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Listening to port ${port}`)
 })
