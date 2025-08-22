b737722b6bd15078bd80ec1bfe0f4f15dc58b036
Kristoffer Johansson <56152114+Krillinator@users.noreply.github.com>
2025-08-22 08:59:06 +0200

    Refactor: Clean Code

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,13 @@ import express from "express"
 import { env } from "node:process"
 
 const app = express()
-const port: number = Number(env.PORT) || 3000 // Could crash
+const port: number = Number(env.PORT) || 3000 // Possibility for Crash
+const address: string = "0.0.0.0" // Localhost - required for Render
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello world" })
 })
 
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, address, () => {
   console.log(`Listening to port ${port}`)
 })
